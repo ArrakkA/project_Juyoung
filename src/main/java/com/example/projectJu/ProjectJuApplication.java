80363aadb4167a6467bc5af46f9a1f770828e656
DESKTOP-S35E8BD\kys <kys@greenit.cc>
2023-01-04 21:40:13 +0900

    first commit

---
+++ src/main/java/com/example/projectJu/ProjectJuApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectJu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJuApplication.class, args);
+	}
+
+}
